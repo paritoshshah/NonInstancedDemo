c9b96a085badca6862e1e35b6c5e923b05ebbd71
Paritosh Shah <paritosh@pocketgems.com>
2014-08-21 16:51:47 -0700

    Initial Commit

---
+++ NonInstancedDemo/Classes/NonInstancedDemoLayer.h
@@ -0,0 +1,17 @@
+/**
+ *  NonInstancedDemoLayer.h
+ *  NonInstancedDemo
+ *
+ *  Created by Paritosh Shah on 8/21/14.
+ *  Copyright Paritosh Shah 2014. All rights reserved.
+ */
+
+
+#import "CC3Layer.h"
+
+
+/** A sample application-specific CC3Layer subclass. */
+@interface NonInstancedDemoLayer : CC3Layer {
+}
+
+@end
